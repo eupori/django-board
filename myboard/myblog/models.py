6f3c7c9abffe1189f33be610f8f33e5c5c9f50f2
이채민 <cmlee-intern@insilicogen.com>
2021-04-21 09:57:24 +0900

    make post

--- myboard/myblog/models.py
+++ myboard/myblog/models.py
@@ -4,4 +4,32 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    pass
+    title = models.CharField(verbose_name='제목', max_length=50)
+    slug = models.SlugField('SLUG', unique=True, allow_unicode=True, help_text='one word for title alias.')
+    description = models.CharField('설명', max_length=100, blank=True, help_text='Simple description text.')
+    content = models.TextField('본문')
+    created_dt = models.DateTimeField('생성일', auto_now=True)
+    modify_dt = models.DateTimeField('수정일', auto_now=True)
+    is_active = models.BooleanField('활성화 여부', default=True)
+    deleted_at = models.DateTimeField('삭제일', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'post'
+        db_table = 'blog_posts'
+        ordering = ('-modify_dt',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('myblog:post_detail', args=(self.slug,))
+
+    def get_previous(self):
+        return self.get_previous_by_modify_dt()
+
+    def get_next(self):
+        return self.get_next_by_modify_dt()
+
+
+
--- myboard/myblog/views/views.py
+++ myboard/myblog/views/views.py
@@ -1,7 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
+from myblog.models import Post
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("cmlee's first blog in django")
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostArchiveView(ArchiveIndexView):
+    model = Post
+    template_name = 'blog/post_archive.html'
+    date_field = 'modify_dt'
+
+
+class PostYearArchiveView(YearArchiveView):
+    model = Post
+    template_name = 'blog/post_archive_year.html'
+    date_field = 'modify_dt'
+    make_object_list = True
+
+
+class PostMonthArchiveView(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostDayArchiveView(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostTodayArchiveView(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+
+
+
+
+

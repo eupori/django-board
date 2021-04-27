60de87427c76addcdb671db9925659fbfbf8876a
이채민 <cmlee-intern@insilicogen.com>
2021-04-27 17:28:27 +0900

    binding error fix

--- myboard/water/admin.py
+++ myboard/water/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Water
 # Register your models here.
+
+@admin.register(Water)
+class WaterAdmin(admin.ModelAdmin):
+	list_display = ('id', 'time', 'amount')

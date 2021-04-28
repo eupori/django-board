bdbf49da1193391e29940933075c0cd48331c18c
이채민 <cmlee-intern@insilicogen.com>
2021-04-28 11:16:37 +0900

    Water 메뉴, admin 추가

--- myboard/water/admin.py
+++ myboard/water/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Water
+from .models import Water, Profile
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'goal', 'total')
 
 @admin.register(Water)
 class WaterAdmin(admin.ModelAdmin):

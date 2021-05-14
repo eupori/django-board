c9d9e5a81783434365ea6d034d08e7d9323d3242
이채민 <cmlee-intern@insilicogen.com>
2021-05-14 10:29:44 +0900

    add datepicker

--- myboard/water/views/views.py
+++ myboard/water/views/views.py
@@ -8,9 +8,10 @@ from django.shortcuts import render
 from water.models import User, Profile, Water
 from rest_framework import serializers
 from django.template.loader import render_to_string
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import datetime
 import json
-
 
 class WaterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,8 +25,49 @@ class UserSerializer(serializers.ModelSerializer):
         fields="__all__"
 
 
-class WaterView(TemplateView):
+def get_chart_data(user, water):
+    # today_amount = sum([item.amount for item in water])
+    day_7 = datetime.datetime.now()-datetime.timedelta(days=7)
+    water_today = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
+    water_week = Water.objects.filter(user=user, time__gte=day_7.date()).order_by('-time')
+
+    
+
+    today_json = {}
+    week_json = {}
+
+    for each in water_week:
+        if each.time.date() in week_json.keys():
+            week_json[each.time.date()] += each.amount
+        else:
+            week_json[each.time.date()] = each.amount
+    week_list = [[str(key), week_json[key]] for key in sorted(week_json.keys())]
+
+    for each in water_today:
+        if each.time.hour in today_json.keys():
+            today_json[each.time.hour] += each.amount
+        else:
+            today_json[each.time.hour] = each.amount
+    today_list = [[str(key), today_json[key]] for key in sorted(today_json.keys())]
+
+    week_values = list(water_week.values_list("amount", flat=True))
+    week_avg = sum(week_values) / len(week_list)
+
+    print("@@@@@@@@@@@@@@@@@@@")
+    print(week_values)
+    print(sum(week_values))
+    print(len(week_values))
+    print(len(week_list))
+    print("@@@@@@@@@@@@@@@@@@@")
+    
+
+    return {"today_list":today_list, "week_list":week_list, "week_avg":week_avg}
+
+
+class WaterView(LoginRequiredMixin, TemplateView):
     template_name = 'templates.html'
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
     # request
     def get_context_data(self, **kwargs):
         kwargs.setdefault('view', self)
@@ -38,32 +80,22 @@ class WaterView(TemplateView):
         water_today = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
         water_week = Water.objects.filter(user=user, time__gte=day_7.date()).order_by('-time')
 
-        today_json = {}
-        week_json = {}
-        today_amount = 0
+        water = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
+        today_amount = sum(list(water.values_list("amount", flat=True)))
 
-        for each in water_week:
-            if each.time.date() in week_json.keys():
-                week_json[each.time.date()] += each.amount
-            else:
-                week_json[each.time.date()] = each.amount
-        week_list = [[str(key), week_json[key]] for key in sorted(week_json.keys())]
+        water_json = WaterSerializer(water, many=True).data
+        user_json = UserSerializer(user).data
 
-        for each in water_today:
-            if each.time.hour in today_json.keys():
-                today_json[each.time.hour] += each.amount
-            else:
-                today_json[each.time.hour] = each.amount
-            today_amount += each.amount
-        today_list = [[str(key), today_json[key]] for key in sorted(today_json.keys())]
+        chart_data = get_chart_data(user, water)
 
         context = {}
         context['water'] = water_today
         context['object'] = Profile.objects.last()
         context['today'] = datetime.datetime.today()
         context['today_amount'] = today_amount
-        context['today_list'] = json.dumps(today_list)
-        context['week_list'] = json.dumps(week_list)
+        context['today_list'] = json.dumps(chart_data['today_list'])
+        context['week_list'] = json.dumps(chart_data['week_list'])
+        context['week_avg'] = int(chart_data['week_avg'])
         return context
 
 
@@ -76,37 +108,18 @@ class WaterView(TemplateView):
         user.save()
 
         water = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
+        today_amount = sum(list(water.values_list("amount", flat=True)))
+
         water_json = WaterSerializer(water, many=True).data
         user_json = UserSerializer(user).data
 
-        today_amount = sum(list(water.values_list("amount", flat=True)))
-        # today_amount = sum([item.amount for item in water])
+        chart_data = get_chart_data(user, water)
 
-        water_today = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
-        water_week = Water.objects.filter(user=user, time__gte=(datetime.datetime.now()+datetime.timedelta(days=7)).date()).order_by('-time')
-
-        today_json = {}
-        week_json = {}
-
-        for each in water_week:
-            if each.time.date in week_json.keys():
-                week_json[each.time.date] += each.amount
-            else:
-                week_json[each.time.date] = each.amount
-        week_list = [[str(key), week_json[key]] for key in sorted(week_json.keys())]
-
-        for each in water_today:
-            if each.time.hour in today_json.keys():
-                today_json[each.time.hour] += each.amount
-            else:
-                today_json[each.time.hour] = each.amount
-
-        today_list = [[str(key), today_json[key]] for key in sorted(today_json.keys())]
-
-        result = {'success':"success", 'water':water_json, 'object':user_json, 'today':str(datetime.datetime.today()), 'today_amount':today_amount, 'today_list':today_list}
+        result = {'success':"success", 'water':water_json, 'object':user_json,
+                'today':str(datetime.datetime.today()), 'today_amount':today_amount, 'today_list':chart_data['today_list']}
         render_page = render_to_string("history.html", {'water':water})
-        render_page_chart_today = render_to_string("today_chart.html", {'today_list':json.dumps(today_list)})
-        render_page_chart_week = render_to_string("week_chart.html", {'week_list':json.dumps(week_list)})
+        render_page_chart_today = render_to_string("today_chart.html", {'today_list':json.dumps(chart_data['today_list'])})
+        render_page_chart_week = render_to_string("week_chart.html", {'week_list':json.dumps(chart_data['week_list']), 'week_avg':int(chart_data['week_avg'])})
         return JsonResponse({'result':result, 'render_history':render_page, 'render_chart_today':render_page_chart_today, 'render_chart_week':render_page_chart_week})
 
 class WaterDeleteView(TemplateView):
@@ -118,35 +131,16 @@ class WaterDeleteView(TemplateView):
 
         user = target_object.user
         water = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
+        today_amount = sum(list(water.values_list("amount", flat=True)))
+
         water_json = WaterSerializer(water, many=True).data
         user_json = UserSerializer(user).data
 
-        today_amount = sum(list(water.values_list("amount", flat=True)))
-        # today_amount = sum([item.amount for item in water])
+        chart_data = get_chart_data(user, water)
 
-        water_today = Water.objects.filter(user=user, time__gte=datetime.datetime.today().date()).order_by('-time')
-        water_week = Water.objects.filter(user=user, time__gte=(datetime.datetime.now()+datetime.timedelta(days=7)).date()).order_by('-time')
-
-        today_json = {}
-        week_json = {}
-
-        for each in water_week:
-            if each.time.date in week_json.keys():
-                week_json[each.time.date] += each.amount
-            else:
-                week_json[each.time.date] = each.amount
-        week_list = [[str(key), week_json[key]] for key in sorted(week_json.keys())]
-
-        for each in water_today:
-            if each.time.hour in today_json.keys():
-                today_json[each.time.hour] += each.amount
-            else:
-                today_json[each.time.hour] = each.amount
-
-        today_list = [[str(key), today_json[key]] for key in sorted(today_json.keys())]
-
-        result = {'success':"success", 'water':water_json, 'object':user_json, 'today':str(datetime.datetime.today()), 'today_amount':today_amount, 'today_list':today_list}
+        result = {'success':"success", 'water':water_json, 'object':user_json,
+                'today':str(datetime.datetime.today()), 'today_amount':today_amount, 'today_list':chart_data['today_list']}
         render_page = render_to_string("history.html", {'water':water})
-        render_page_chart_today = render_to_string("today_chart.html", {'today_list':json.dumps(today_list)})
-        render_page_chart_week = render_to_string("week_chart.html", {'week_list':json.dumps(week_list)})
+        render_page_chart_today = render_to_string("today_chart.html", {'today_list':json.dumps(chart_data['today_list'])})
+        render_page_chart_week = render_to_string("week_chart.html", {'week_list':json.dumps(chart_data['week_list']), 'week_avg':int(chart_data['week_avg'])})
         return JsonResponse({'result':result, 'render_history':render_page, 'render_chart_today':render_page_chart_today, 'render_chart_week':render_page_chart_week})
